1d04b90717190a518b42e3ca91e6c8ad4c05f19b
pxd <pxd@hua-cloud.com.cn>
2019-02-27 15:32:14 +0800

    注释脚本

--- FloatWord_start.bat
+++ FloatWord_start.bat
@@ -1,3 +1,4 @@
+::win启动脚本，根据需要修改路径即可
 %1 mshta vbscript:CreateObject("WScript.Shell").Run("%~s0 ::",0,FALSE)(window.close)&&exit
 
 java -jar ./floatword-1.0-SNAPSHOT.jar F:\IdeaWorkSpace\floatword\word.csv
